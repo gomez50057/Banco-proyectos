f9b0044f8cb77dd93b52c417451732eac545c3c5
gomez50057 <gomez.50057@gmail.com>
2024-06-26 00:20:01 -0600

    a

--- banco_pro/serializers.py
+++ banco_pro/serializers.py
@@ -2,6 +2,8 @@
 
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import FormProject
+
 
 class UsuarioSerializer(serializers.ModelSerializer):
     tipo_cuenta = serializers.CharField()
@@ -15,22 +17,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username']
 
-
-
-
-from rest_framework import serializers
-from .models import FormProject
-
 class FormProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = FormProject
         fields = '__all__'  # Incluye todos los campos del modelo
-
-
-from rest_framework import serializers
-from .models import FormProject
-
-class FormProjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = FormProject
-        fields = '__all__'
