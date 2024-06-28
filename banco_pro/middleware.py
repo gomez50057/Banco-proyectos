cbe8f48b3387a7a6d9f94f32e901a7e1cfb977b1
gomez50057 <gomez.50057@gmail.com>
2024-06-27 23:43:02 -0600

    base

--- banco_pro/middleware.py
+++ banco_pro/middleware.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 
 def admin_required(function=None, redirect_field_name=None, login_url=None):
     actual_decorator = user_passes_test(
-        lambda u: u.is_authenticated and u.groups.filter(name='admin').exists(),
+        lambda u: u.is_authenticated and u.groups.filter(name='responsable').exists(),
         login_url=login_url,
         redirect_field_name=redirect_field_name
     )
@@ -17,7 +17,7 @@ def admin_required_middleware(get_response):
     def middleware(request):
         if request.path == '/crud/' and not request.user.is_authenticated:
             return redirect(reverse('login'))
-        if request.path == '/crud/' and not request.user.groups.filter(name='admin').exists():
+        if request.path == '/crud/' and not request.user.groups.filter(name='responsable').exists():
             return redirect(reverse('login'))
         return get_response(request)
     return middleware
