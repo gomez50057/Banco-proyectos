cc46317e2f7a2366d09f0048c4ce990c8303e7d6
gomez50057 <gomez.50057@gmail.com>
2024-08-12 17:47:55 -0600

    form datos

--- banco_pro/models.py
+++ banco_pro/models.py
@@ -3,6 +3,20 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class FormProject(models.Model):
+
+    # Campos del Paso 1
+    nombre_dependencia = models.CharField(max_length=255, default="")
+    area_adscripcion = models.CharField(max_length=255, default="")
+    nombre_registrante = models.CharField(max_length=255, default="")
+    apellido_paterno = models.CharField(max_length=255, default="")
+    apellido_materno = models.CharField(max_length=255, default="")
+    correo_institucional = models.EmailField(max_length=255, default="")
+    telefono_oficina = models.CharField(max_length=10, default="")
+    telefono_oficina_ext = models.CharField(max_length=10, null=True, blank=True, default="")
+    correo_personal = models.EmailField(max_length=255, default="")
+    telefono_particular = models.CharField(max_length=10, default="")
+
+
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
     project_id = models.CharField(max_length=50, unique=True, blank=True, null=True)
     fecha_registro = models.DateField(auto_now_add=True)
